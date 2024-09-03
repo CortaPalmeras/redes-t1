785bd22a25b9b043f896f8516f0bb02fb66acdfa
Lucas Llort <lucasllort@gmail.com>
2024-09-03 15:27:39 -0400

    elim tests

--- tests/query_validation.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-import src.query_validation as qv
-
-class Test(unittest.TestCase):
-
-    def test_correct_type(self):
-        self.assertIsInstance(qv.validate_names(["lucas", "llort", "bonnefont"]), qv.ValidQuery)
-
